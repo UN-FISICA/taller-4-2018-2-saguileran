ea430003942107fb596ca3eebfa673f4edb91bb7
Sebastian <saguileran@unal.edu.co>
2018-10-07 22:21:38 -0500

    Finalizando taller dederivadas

---
+++ taller.py
@@ -0,0 +1,97 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from sympy import *
+from scipy import optimize
+import numpy as np
+#from math import *
+
+class Derivada:
+    def __init__(self, f, metodo, dx=1e-1):
+        self.f=f
+        self.dx=float(dx)
+        self.metodo=metodo
+    #def __call__(self, x):
+     #   f, dx = self.f, self.dx      # make short forms
+      #  return (f(x+dx) - f(x))/dx
+    def calc(self,x):        
+        if self.metodo=='adelante':
+            return((-float(self.f(x))+float(self.f(x+self.dx)))/self.dx)
+        elif self.metodo=='central':
+            return((float(self.f(x+(self.dx)/2))-float(self.f(x-(self.dx)/2)))/(self.dx))
+        elif self.metodo=='extrapolada':
+            dca=(float(self.f(x+self.dx/4))-float(self.f(x-self.dx/4)))/(self.dx/2)
+            dcn=(float(self.f(x+self.dx/2))-float(self.f(x-self.dx/2)))/self.dx
+            return((4*dca-dcn)/3)
+        elif self.metodo=='segunda':
+            #Segunda derivada
+            return(float(self.f(x+self.dx)+self.f(x-self.dx)-2*self.f(x))/(self.dx**2))
+        else:return('Método no válido')
+  ##Núero de ceros en dx, a izquierda, igual a numero de digitos aproximados
+        """ Método que retorna el valor numérico de la derivada de la
+    función f evaluada en el punto x"""
+#Mejorar metodos
+#x = Symbol('x')
+
+class Zeros:
+    def __init__(self, f, metodo, error=1e-4, max_iter=100):
+        self.f=f
+        self.metodo=metodo
+        self.error=float(error)
+        self.max_inter=float(max_iter)
+
+    def zero(self,vi): 
+        #Newton
+        def Newton(vi):
+            i,vi=0,float(vi)
+            while abs(float(self.f(vi)))>self.error:
+                F=Derivada(self.f,'extrapolada',1e-6)
+                vi=vi-(float(self.f(vi))/F.calc(vi))
+                i+=1
+                if i==self.max_inter: break
+            return(vi)
+        #Bisectriz
+        def Bisection(x):
+            a,b=x[0],x[1]
+            if float(self.f(b))*float(self.f(a))<0:
+                a,b=float(vi[0]),float(vi[1])
+                c,i=(a+b)/2,0
+                while abs(float(self.f(c)))>self.error:
+                    if i==self.max_inter: break
+                    elif float(self.f(a))*float(self.f(c))<0:b=c
+                    else: a=c
+                    c=(a+b)/2
+                    i+=1
+                return(c)
+            else: 'Datos ingresados mal'
+        #Interpolación lineal
+        def Interpol(vi):
+                a,b=float(vi[0]),float(vi[1])
+                c,i=(float(self.f(b))*a-float(self.f(a))*b)/(float(self.f(b))-float(self.f(a))),0
+                while abs(float(self.f(c)))>self.error:
+                    if i==self.max_inter: break
+                    elif float(self.f(a))*float(self.f(c))<0:b=c
+                    else: a=c
+                    c=(float(self.f(b))*a-float(self.f(a))*b)/(float(self.f(b))-float(self.f(a)))
+                    i+=1
+                return(c)
+            
+        if self.metodo=='newton':return(print(Newton(vi)))
+        elif self.metodo=='bisectriz':return(print(Bisection(vi)))
+        elif self.metodo=='interpolacion':return(print(Interpol(vi)))
+        elif self.metodo=='newton-sp':
+            return(print(optimize.newton(self.f,vi,prime=None, tol=self.error, maxiter=self.max_inter, fprime2=None))) #Calcula la derivada con el método de la secante
+        elif self.metodo=='fsolve-sp':
+            return(print(optimize.fsolve(self.f,vi,xtol=self.max_inter)))
+        elif self.metodo=='brentq-sp':pass
+        else: print('Método no válido')
+        
+
+    
+if __name__ ==" __main__ ": pass
+else:
+    #F=Derivada(exp,'segunda')
+    #DF=F.calc(1)
+    I=Zeros(sin,'fsolve-sp')
+    I.zero(1.33)
+        

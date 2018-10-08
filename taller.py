4f0db8b2bee23c015ce724685faaf2ec47e288b2
Sebastian <saguileran@unal.edu.co>
2018-10-07 23:23:20 -0500

    Clase de derivadas

--- taller.py
+++ taller.py
@@ -34,13 +34,17 @@ class Derivada:
 #x = Symbol('x')
 
 class Zeros:
-    def __init__(self, f, metodo, error=1e-4, max_iter=100):
+    def __init__(self, f, metodo, error=1e-6, max_iter=100):
         self.f=f
         self.metodo=metodo
         self.error=float(error)
         self.max_inter=float(max_iter)
 
     def zero(self,vi): 
+        if type(vi)==tuple:
+            a=float(vi[0])
+            b=float(vi[1])
+        else: vi=float(vi)
         #Newton
         def Newton(vi):
             i,vi=0,float(vi)
@@ -57,41 +61,61 @@ class Zeros:
                 a,b=float(vi[0]),float(vi[1])
                 c,i=(a+b)/2,0
                 while abs(float(self.f(c)))>self.error:
-                    if i==self.max_inter: break
+                    if float(self.f(c))==0:break
+                    elif i==self.max_inter: break
                     elif float(self.f(a))*float(self.f(c))<0:b=c
                     else: a=c
                     c=(a+b)/2
                     i+=1
                 return(c)
-            else: 'Datos ingresados mal'
+            else: 'Datos iniciales no apropiados'
         #Interpolación lineal
         def Interpol(vi):
                 a,b=float(vi[0]),float(vi[1])
                 c,i=(float(self.f(b))*a-float(self.f(a))*b)/(float(self.f(b))-float(self.f(a))),0
                 while abs(float(self.f(c)))>self.error:
-                    if i==self.max_inter: break
+                    if float(self.f(c))==0:break
+                    elif i==self.max_inter: break
                     elif float(self.f(a))*float(self.f(c))<0:b=c
                     else: a=c
                     c=(float(self.f(b))*a-float(self.f(a))*b)/(float(self.f(b))-float(self.f(a)))
                     i+=1
                 return(c)
-            
-        if self.metodo=='newton':return(print(Newton(vi)))
-        elif self.metodo=='bisectriz':return(print(Bisection(vi)))
-        elif self.metodo=='interpolacion':return(print(Interpol(vi)))
+        #b=float(vi[0]),float(vi[1])
+        if self.metodo=='newton':return((Newton(vi)))
+        elif self.metodo=='bisectriz':return((Bisection(vi)))
+        elif self.metodo=='interpolacion':return((Interpol(vi)))
         elif self.metodo=='newton-sp':
-            return(print(optimize.newton(self.f,vi,prime=None, tol=self.error, maxiter=self.max_inter, fprime2=None))) #Calcula la derivada con el método de la secante
+            return((optimize.newton(self.f,vi,fprime=None, tol=self.error, maxiter=int(self.max_inter), fprime2=None))) #Calcula la derivada con el método de la secante
         elif self.metodo=='fsolve-sp':
-            return(print(optimize.fsolve(self.f,vi,xtol=self.max_inter)))
-        elif self.metodo=='brentq-sp':pass
+            def func(x):
+                return(self.f(x))    
+            #return(print(optimize.fsolve(func, np.array(vi), args=(), fprime=None, full_output=0, col_deriv=0, xtol=self.error, maxfev=int(self.max_inter), band=None, epsfcn=None, factor=100, diag=None)))#self.f,vi,xtol=self.max_inter)))
+            return(optimize.fsolve(self.f,a,xtol=self.error,maxfev=self.max_inter))
+        elif self.metodo=='brentq-sp':
+            return(optimize.brentq(self.f,vi[0],vi[1],xtol=self.error,maxiter=self.max_inter))
+           #return(optimize.brentq(self.f, a, b, args=(), xtol=self.error, rtol=8.881784197001252e-16, maxiter=self.max_inter, full_output=False, disp=True))
+            #return(print(self.f, a,b, args=(), xtol=self.error, rtol=8.881784197001252e-16, maxiter=self.max_inter, full_output=False, disp=True))
         else: print('Método no válido')
         
 
     
 if __name__ ==" __main__ ": pass
 else:
-    #F=Derivada(exp,'segunda')
-    #DF=F.calc(1)
-    I=Zeros(sin,'fsolve-sp')
-    I.zero(1.33)
-        
+    F=Derivada(exp,'segunda')
+    print(F.calc(1))
+    F=Derivada(exp,'central')
+    print(F.calc(1))
+    F=Derivada(exp,'extrapolada')
+    print(F.calc(1))   
+    F=Derivada(exp,'segunda')
+    print(F.calc(1))
+    print()
+    I=Zeros(cos,'newton')
+    print(I.zero(0.22))
+    I=Zeros(cos,'bisectriz')
+    print(I.zero((0,3.22)))   
+    I=Zeros(cos,'interpolacion')
+    print(I.zero((0.22,1.22)))
+    I=Zeros(cos,'newton-sp')
+    print(I.zero(5.23))
